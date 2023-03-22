1396a3c8780d2c95eb47b1c14fa52943db2a6878
cormorecheleo <leocormo@gmail.com>
2023-03-22 14:13:08 +0100

    Mail Feature

---
+++ app/Console/Commands/SendMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\HelloMail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send Mail';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Mail::to("test@test.com")->send(new HelloMail());
+    }
+}
